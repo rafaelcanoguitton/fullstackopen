097ee480b7295a35cd940238cf0fd480fc4a5b28
rafaelcanoguitton <rafael.cano@ucsp.edu.pe>
2021-08-24 11:10:30 -0500

    3.3 done.

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -26,6 +26,15 @@ const phonebook = [
 app.get('/api/persons', (request, response) => {
   response.json(phonebook);
 });
+app.get('/api/persons/:id',(request,response)=>{
+    const id=request.params.id;
+    const person=phonebook.find(p=>p.id==id);
+    if(person){
+        response.json(person);
+    } else {
+        response.status(404).end()
+    }
+});
 app.get(('/info'),(request,response)=>{
     response.send(`<div>Phonebook has info for ${phonebook.length} people</div><br><div>${Date().toLocaleString('en-us')}</div>`)
 });
