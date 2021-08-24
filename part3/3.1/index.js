7fc7cddc06d6825b8f2ad4c74ce54d41f6b71b1e
rafaelcanoguitton <rafael.cano@ucsp.edu.pe>
2021-08-24 11:56:06 -0500

    3.5 done.

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const express = require("express");
 const app = express();
 let phonebook = [
@@ -23,25 +22,46 @@ let phonebook = [
     number: "39-23-6423122",
   },
 ];
-app.get('/api/persons', (request, response) => {
+app.use(express.json());
+app.get("/api/persons", (request, response) => {
   response.json(phonebook);
 });
-app.get('/api/persons/:id',(request,response)=>{
-    const id=request.params.id;
-    const person=phonebook.find(p=>p.id==id);
-    if(person){
-        response.json(person);
-    } else {
-        response.status(404).end()
-    }
+app.post("/api/persons", (request, response) => {
+  const maxId = Math.floor(Math.random() * 1000000);
+  const body = request.body;
+  if (!body.name) {
+    return response.status(400).json({ error: "name missing" });
+  } else if (!body.number) {
+    return response.status(400).json({ error: "number missing" });
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: maxId,
+  };
+  phonebook = phonebook.concat(person);
+  response.json(person);
 });
-app.delete('/api/persons/:id',(request,response)=>{
-    const id=request.params.id;
-    phonebook=phonebook.filter(p=>p.id!=id);
-    response.status(204).end();
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = phonebook.find((p) => p.id == id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
-app.get(('/info'),(request,response)=>{
-    response.send(`<div>Phonebook has info for ${phonebook.length} people</div><br><div>${Date().toLocaleString('en-us')}</div>`)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  phonebook = phonebook.filter((p) => p.id != id);
+  response.status(204).end();
+});
+app.get("/info", (request, response) => {
+  response.send(
+    `<div>Phonebook has info for ${
+      phonebook.length
+    } people</div><br><div>${Date().toLocaleString("en-us")}</div>`
+  );
 });
 const PORT = 3001;
 app.listen(PORT, () => {
