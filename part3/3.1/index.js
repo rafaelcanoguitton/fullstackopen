487f9e83b269281bcc163f55510e663ba72ce652
rafaelcanoguitton <rafael.cano@ucsp.edu.pe>
2021-08-25 12:11:56 -0500

    3.16 done.

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -6,12 +6,19 @@ morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
 const app = express();
-
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
 app.use(
   express.json(),
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
   cors(),
-  express.static("build")
+  express.static("build"),
+  errorHandler
 );
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((people) => response.json(people));
@@ -24,23 +31,29 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({ error: "number missing" });
   }
   const person = new Person({
-    name:body.name,
-    number:body.number,
+    name: body.name,
+    number: body.number,
   });
-  person.save().then(result=>{
+  person.save().then((result) => {
     console.log(result);
     response.json(result);
   });
 });
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  Person.findById(id).then((p) => response.json(p));
+  Person.findById(id)
+    .then((p) => response.json(p))
+    .catch((error) => {
+      next(error);
+    });
 });
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  Person.findByIdAndRemove(id).then((res=>{
-    response.status(204).end();
-  }));
+  Person.findByIdAndRemove(id)
+    .then((res) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 app.get("/info", (request, response) => {
   response.send(
@@ -49,7 +62,7 @@ app.get("/info", (request, response) => {
     } people</div><br><div>${Date().toLocaleString("en-us")}</div>`
   );
 });
-const PORT = process.env.PORT||3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
