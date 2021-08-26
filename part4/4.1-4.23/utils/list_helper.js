168f17cb06ac85e063a2ca254fc3c5b5ded30d2e
rafaelcanoguitton <rafael.cano@ucsp.edu.pe>
2021-08-25 23:21:06 -0500

    4.4 done.

--- part4/4.1-4.23/utils/list_helper.js
+++ part4/4.1-4.23/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
   return 1;
 };
-
+const totalLikes=(blogs)=>{
+  const reducer=(sum,item)=>{
+    return sum + item.likes;
+  }
+  if(blogs.length===0){
+    return 0;
+  }
+  return blogs.reduce(reducer,0);
+}
 module.exports = {
   dummy,
+  totalLikes
 };
