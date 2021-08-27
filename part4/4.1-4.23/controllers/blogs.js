f1d95cd7f850d0c7ce55072b700ae753e8dc7c93
rafaelcanoguitton <rafael.cano@ucsp.edu.pe>
2021-08-26 22:55:56 -0500

    4.10 done.

--- part4/4.1-4.23/controllers/blogs.js
+++ part4/4.1-4.23/controllers/blogs.js
@@ -1,16 +1,15 @@
 const blogRouter = require("express").Router();
 const { result } = require("lodash");
 const Blog = require("../models/blog");
-require('express-async-errors')
-blogRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+require("express-async-errors");
+blogRouter.get("/", async (request, response) => {
+  const blogs=await Blog.find({});
+  response.json(blogs);
 });
 
-blogRouter.post("/",async (request, response) => {
+blogRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
-  result= await blog.save();
+  const result = await blog.save();
   response.status(201).json(result);
 });
-module.exports=blogRouter;
+module.exports = blogRouter;
--- part4/4.1-4.23/tests/blog_api.test.js
+++ part4/4.1-4.23/tests/blog_api.test.js
@@ -4,6 +4,7 @@ const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
 const helper = require("./test_helper");
+require("express-async-errors");
 beforeEach(async () => {
   await Blog.deleteMany({});
   await Blog.insertMany(helper.initialBlogs);
@@ -13,9 +14,35 @@ describe("blog api", () => {
     const result = await api.get("/api/blogs");
     expect(result.body).toHaveLength(helper.initialBlogs.length);
   });
-  test("correct id property",async()=>{
-    const result= await api.get("/api/blogs");
+  test("correct id property", async () => {
+    const result = await api.get("/api/blogs");
     expect(result.body[0].id).toBeDefined();
+  });
+  test("a new blog gets successfully created", async () => {
+    const newBlog = {
+      title: "Drink water",
+      author: "Rafael",
+      url: "some_url",
+      likes: 5
+    };
+    const expectedReturn = {
+      title: newBlog.title,
+      author: newBlog.author,
+    };
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+    const response = await api.get("/api/blogs");
+    const contents = response.body.map((r) => {
+      return {
+        title: r.title,
+        author: r.author,
+      };
+    });
+    expect(contents).toHaveLength(helper.initialBlogs.length + 1);
+    expect(contents).toContainEqual(expectedReturn);
   });
 });
 afterAll(() => {
