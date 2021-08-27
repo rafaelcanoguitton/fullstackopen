117120808c2e757921e628e62d999d59b2c6590f
rafaelcanoguitton <rafael.cano@ucsp.edu.pe>
2021-08-26 21:33:58 -0500

    4.8 done.

--- part4/4.1-4.23/controllers/blogs.js
+++ part4/4.1-4.23/controllers/blogs.js
@@ -1,16 +1,16 @@
 const blogRouter = require("express").Router();
+const { result } = require("lodash");
 const Blog = require("../models/blog");
+require('express-async-errors')
 blogRouter.get("/", (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs);
   });
 });
 
-blogRouter.post("/", (request, response) => {
+blogRouter.post("/",async (request, response) => {
   const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  result= await blog.save();
+  response.status(201).json(result);
 });
 module.exports=blogRouter;
---
+++ part4/4.1-4.23/tests/blog_api.test.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const Blog = require("../models/blog");
+const helper = require("./test_helper");
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+}, 10000);
+describe("blog api", () => {
+  test("correct ammount of posts", async () => {
+    const result = await api.get("/api/blogs");
+    expect(result.body).toHaveLength(helper.initialBlogs.length);
+  });
+});
+afterAll(() => {
+  mongoose.connection.close();
+});
