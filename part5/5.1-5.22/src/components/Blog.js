99cc3094eb4219435925c1f4be764854abfdd9c2
rafaelcanoguitton <rafael.cano@ucsp.edu.pe>
2021-08-29 18:45:39 -0500

    5.7 done.

--- part5/5.1-5.22/src/components/Blog.js
+++ part5/5.1-5.22/src/components/Blog.js
@@ -1,8 +1,18 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showInfo,setShow]=useState(false);
+  if(showInfo){
+    return <div className="blog">
+      {blog.title}<button onClick={()=>{setShow(!showInfo)}}>hide</button><br/>
+      {blog.url}<br/>
+      {blog.likes} <button>like</button><br/>
+      {blog.author}<br/>
+    </div>
+  }else{
+    return <div className="blog">
+    {blog.title} {blog.author} <button onClick={()=>{setShow(!showInfo)}}>view</button>
+  </div>;
+  }
+};
 
-export default Blog
+export default Blog;
