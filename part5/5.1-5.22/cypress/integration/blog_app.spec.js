31372824e7641fbca3f1d6145d2ea2d69f9ad7ee
rafaelcanoguitton <rafael.cano@ucsp.edu.pe>
2021-08-30 14:15:19 -0500

    5.18 done.

--- part5/5.1-5.22/cypress/integration/blog_app.spec.js
+++ part5/5.1-5.22/cypress/integration/blog_app.spec.js
@@ -1,10 +1,29 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST','http://localhost:3003/api/users/',{
+        username:'Rafael',
+        password:'Fullstack'
+    })
     cy.visit('http://localhost:3000')
   })
   it('Login form is shown', function () {
     cy.contains('username')
     cy.contains('password')
+  })
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+        cy.get('input:first').type('Rafael')
+        cy.get('input:last').type('Fullstack')
+        cy.contains('login').click()
+        cy.contains('logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+        cy.get('input:first').type('test')
+        cy.get('input:last').type('nouser')
+        cy.contains('login').click()
+        cy.contains('Wrong username or password')
+    })
   })
 })
