3373fcff8e99ebf9b68b73b08e29ae0ac5036830
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-11-29 10:15:20 -0500

    9.9 done.

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 const app = express();
+const cors = require("cors");
 app.use(express.json());
-const PORT = 3000;
-app.get("/ping", (_req, res) => {
+app.use(cors());
+const PORT = 3001;
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
