9dc58c1e2b6763884510717d858e9ca6807a81ee
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-12-08 15:16:33 -0500

    9.20 done.

--- part9/patientor/src/components/Patient.tsx
+++ part9/patientor/src/components/Patient.tsx
@@ -3,9 +3,29 @@ import { useParams } from "react-router";
 import { Icon } from "semantic-ui-react";
 import { useStateValue } from "../state";
 import { Patient } from "../types";
+import { Entry } from "../types";
+const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+  console.log("wadafa");
+  return (
+    <div>
+      <div>
+        <span>
+          {entry.date} {entry.description}
+        </span>
+      </div>
+      <div>
+        <ul>
+          {entry.diagnosisCodes?.map((code) => (
+            <li key={code}>{code}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 const PatientPage = () => {
   const { id } = useParams<{ id: string }>();
-  const [{ patients },] = useStateValue();
+  const [{ patients }] = useStateValue();
   const patient: Patient = patients[id];
   return (
     <>
@@ -15,6 +35,10 @@ const PatientPage = () => {
       </h1>
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+      <h2>entries</h2>
+      {patient.entries?.map((entry,idx) => (
+        <EntryDetails key={idx} entry={entry} />
+      ))}
     </>
   );
 };
--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -9,7 +9,39 @@ export enum Gender {
   Female = "female",
   Other = "other"
 }
-
+export enum HealthCheckRating{
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+export type Entry = HospitalEntry | OccupationalHealthCareEntry | HealthCheckEntry;
 export interface Patient {
   id: string;
   name: string;
@@ -17,4 +49,5 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries:Entry[];
 }
